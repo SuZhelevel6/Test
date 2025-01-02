5c0c72f3850ab1da1c4a4e7c0b1802fa54d843e8
Zhang Tao <zhangt@giec.cn>
2025-01-02 09:50:30 +0800

    创建SumApplication.kt Application 类文件

---
+++ app/src/main/java/com/sum/tea/SumApplication.kt
@@ -0,0 +1,18 @@
+package com.sum.tea
+
+import android.app.Application
+import android.content.Context
+
+
+class SumApplication  : Application() {
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
